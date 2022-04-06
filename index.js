437220f082810f040b720c75749d8d2bed0999b5
Courtney Garcia <wavesofgrace3@gmail.com>
2022-04-06 08:23:06 -0700

    commit tree 6d8e46ffa3f377d4c7e77148c7b8a5346e2d71ec

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+customerName = 'bob'
+const leastFavoriteCustomer = 'Jerry'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Daniel';
+}
